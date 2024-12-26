2055f0ca4fc6cbb1aba4c1b63f97f74027a333e1
fisheeesh <swanphyo444@gmail.com>
2024-12-26 15:22:08 +0700

    project setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-indigo-700">HI</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/router/index.jsx
@@ -0,0 +1,20 @@
+import {
+    createBrowserRouter
+} from "react-router-dom";
+import Layout from "../pages/layout/Layout";
+import Home from '../pages/Home'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            }
+        ]
+    },
+]);
+
+export default router
