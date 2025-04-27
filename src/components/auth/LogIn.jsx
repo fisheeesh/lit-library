e6575eefcc867030da7ea3b8c4169aa521b9bad9
fisheeesh <swanphyo444@gmail.com>
2025-04-27 23:23:50 +0700

    fix: disabled form btns

--- src/components/auth/LogIn.jsx
+++ src/components/auth/LogIn.jsx
@@ -43,7 +43,7 @@ export default function LogIn() {
                     <label className={`block mb-2 text-sm font-bold ${isDark ? 'text-white' : 'text-gray-700'}`} htmlFor="email">
                         Email <span className="text-red-600">*</span>
                     </label>
-                    <input {...register('email')} className={cn(errors.email && 'border-red-500 placeholder:text-red-500', 'w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline')} id="email" type="text" placeholder="Email" />
+                    <input disabled={loading} {...register('email')} className={cn(errors.email && 'border-red-500 placeholder:text-red-500', 'w-full px-3 py-2 leading-tight text-gray-700 border disabled:cursor-not-allowed rounded shadow appearance-none focus:outline-none focus:shadow-outline')} id="email" type="text" placeholder="Email" />
                     {errors.email &&
                         <div className="flex items-center gap-1 mt-1">
                             <span className="text-[16px] text-red-600 material-symbols-outlined">
@@ -57,7 +57,7 @@ export default function LogIn() {
                     <label className={`block mb-2 text-sm font-bold ${isDark ? 'text-white' : 'text-gray-700'}`} htmlFor="password">
                         Password <span className="text-red-600">*</span>
                     </label>
-                    <input {...register('password')} className={cn(errors.password && 'border-red-500 placeholder:text-red-500', 'w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline')} id="password" type={`${pVisible ? 'text' : 'password'}`} placeholder="Password" />
+                    <input disabled={loading} {...register('password')} className={cn(errors.password && 'border-red-500 placeholder:text-red-500', 'w-full px-3 py-2 leading-tight text-gray-700 border disabled:cursor-not-allowed rounded shadow appearance-none focus:outline-none focus:shadow-outline')} id="password" type={`${pVisible ? 'text' : 'password'}`} placeholder="Password" />
                     <span onClick={() => setPVisible(prevState => !prevState)} className="absolute text-gray-400 cursor-pointer material-symbols-outlined right-5 top-9">
                         {pVisible ? 'visibility' : 'visibility_off'}
                     </span>
@@ -71,7 +71,7 @@ export default function LogIn() {
                     }
                 </div>
                 <div className="flex items-center justify-between">
-                    <button disabled={loading} className="flex items-center gap-1 px-4 py-2 font-bold text-white transition duration-500 ease-in-out rounded bg-primary hover:bg-blue-700 focus:outline-none focus:shadow-outline" type="submit">
+                    <button disabled={loading} className="flex items-center gap-1 px-4 py-2 font-bold text-white transition duration-500 ease-in-out rounded disabled:cursor-not-allowed bg-primary hover:bg-blue-700 focus:outline-none focus:shadow-outline" type="submit">
                         {loading && <svg className="w-5 h-5 mr-3 -ml-1 text-white animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                             <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                             <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
