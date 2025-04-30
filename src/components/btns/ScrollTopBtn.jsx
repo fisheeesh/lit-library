2637a9c6d382af48807ac3deb8ba267b542c1e02
fisheeesh <swanphyo444@gmail.com>
2025-04-30 23:32:20 +0700

    feat: fixed scroll to top btn

--- src/components/btns/ScrollTopBtn.jsx
+++ src/components/btns/ScrollTopBtn.jsx
@@ -32,11 +32,11 @@ export default function ScrollTopBtn() {
                 type="button"
                 id="to-top"
                 className={`${showBtn ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'} 
-    w-[50px] h-[50px] transition-all duration-200 rounded-full outline-none cursor-pointer 
+    w-[50px] h-[50px] rounded-full outline-none cursor-pointer 
     flex fixed bottom-5 right-5 z-50 items-center justify-center to-top-btn 
     ${isDark
-                        ? 'bg-secondary hover:bg-[#9e1f54] border-b-[6px] border-b-[#80173f] group-hover:bg-[#9e1f54] group-hover:border-t-[6px] group-hover:border-t-[#9e1f54] group-hover:border-b-0 group-hover:border-b-transparent'
-                        : 'bg-primary hover:bg-indigo-800 border-b-[6px] border-b-indigo-800 group-hover:bg-indigo-800 group-hover:border-t-[6px] group-hover:border-t-indigo-800 group-hover:border-b-0 group-hover:border-b-transparent'}`}>
+                        ? 'bg-secondary hover:bg-[#9e1f54] border-b-[5px] border-b-[#80173f] group-hover:bg-[#9e1f54] group-hover:border-t-[5px] group-hover:border-t-[#9e1f54] group-hover:border-b-0 group-hover:border-b-transparent'
+                        : 'bg-primary hover:bg-indigo-800 border-b-[5px] border-b-indigo-800 group-hover:bg-indigo-800 group-hover:border-t-[5px] group-hover:border-t-indigo-800 group-hover:border-b-0 group-hover:border-b-transparent'}`}>
                 <img className="mb-1 w-[18px]" src={upArrow} alt="back-to-top" />
             </button>
         </div>
